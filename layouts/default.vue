df6f3d6e420458787d8dc160c756153bafb2ffe2
Ernesto <ernesto.yanac@deister.es>
2020-06-18 17:29:36 +0200

    Navigation bar is a permanent element

--- layouts/default.vue
+++ layouts/default.vue
@@ -25,48 +25,12 @@
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
-    <v-app-bar :clipped-left="clipped" fixed app>
-      <v-toolbar-title v-text="title" />
-      <v-spacer />
-      <v-btn depressed>
-        <v-icon left>mdi-magnify</v-icon>
-        Search
-      </v-btn>
-      <v-btn depressed>
-        <v-icon>mdi-forum</v-icon>
-      </v-btn>
-      <v-btn depressed>
-        <v-icon>{{
-          notifications ? 'mdi-bell-alert' : 'mdi-bell-outline'
-        }}</v-icon>
-      </v-btn>
-      <!--  user information  -->
-      <v-divider inset vertical></v-divider>
-      <v-col class="hidden-xs-only" sm="1" md="1">
-        <strong v-html="user.name"></strong>
-      </v-col>
-      <v-avatar size="36px">
-        <img v-if="user.avatar" alt="Avatar" :src="user.avatar" />
-        <v-icon v-else :color="user.color" v-text="user.icon"></v-icon>
-      </v-avatar>
-    </v-app-bar>
+    <AppBar />
     <v-main>
       <v-container>
         <nuxt />
       </v-container>
     </v-main>
-    <v-navigation-drawer v-model="rightDrawer" :right="right" temporary fixed>
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
     <v-footer :fixed="fixed" app>
       <span>&copy; {{ new Date().getFullYear() }}</span>
     </v-footer>
@@ -74,6 +38,8 @@
 </template>
 
 <script>
+import AppBar from '@/components/AppBar.vue'
+
 export default {
   data() {
     return {
@@ -81,12 +47,6 @@ export default {
       drawer: false,
       fixed: false,
       notifications: false,
-      user: {
-        avatar: 'https://avatars0.githubusercontent.com/u/9064066?v=4&s=460',
-        name: 'John Doe',
-        color: 'red',
-        icon: 'people'
-      },
       items: [
         {
           icon: 'mdi-apps',
@@ -99,11 +59,11 @@ export default {
           to: '/inspire'
         }
       ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Customer Portal'
+      miniVariant: false
     }
+  },
+  components: {
+    AppBar
   }
 }
 </script>
